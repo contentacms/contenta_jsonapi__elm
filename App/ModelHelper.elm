4e96760fdca25c9e896b106e4b3ebb0fca74c3d7
Daniel Wehner <dawehner@googlemail.com>
2017-05-28 15:08:25 +0200

    Remove unneeded file

--- App/ModelHelper.elm
+++
@@ -1,9 +0,0 @@
-module App.ModelHelper exposing (findFileByUuid)
-
-import App.Model exposing (..)
-import List.Extra
-
-
-findFileByUuid : String -> List File -> Just File
-findFileByUuid uuid =
-    List.Extra.find (\file -> file.uuid == uuid)
