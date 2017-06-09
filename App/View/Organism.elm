e0ff227394022d935e322b12fca13682fb5c591c
Daniel Wehner <dawehner@googlemail.com>
2017-06-09 09:52:31 +0200

    (fix) Move the header into an organism

--- App/View/Organism.elm
+++ App/View/Organism.elm
@@ -3,8 +3,10 @@ module App.View.Organism exposing (..)
 import App.Model exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import App.View.Atom exposing (..)
 import App.View.Molecule exposing (..)
+import App.PageType exposing (..)
 
 
 recipesPerCategory : String -> List Recipe -> Html Msg
@@ -51,3 +53,20 @@ recipesFeaturedHeader =
 recipeMoreArticlesTeaser : Html Msg
 recipeMoreArticlesTeaser =
     text "TODO Implement more articles teaser"
+
+
+viewHeader : Model -> Html Msg
+viewHeader model =
+    div []
+        [ div []
+            []
+          --            [ text "Search"
+          --            ]
+        , h1 [] [ text "Umami, food magazine" ]
+        , ul []
+            [ li [] [ a [ href "#", onClick (SetActivePage Home) ] [ text "Home" ] ]
+            , li [] [ a [ href "#", onClick (SetActivePage ArticleList) ] [ text "Features" ] ]
+            , li [] [ a [ href "#", onClick (SetActivePage RecipesPerCategoryList) ] [ text "Recipes" ] ]
+            , li [] [ a [ href "#", onClick (SetActivePage AboutUs) ] [ text "About us" ] ]
+            ]
+        ]
