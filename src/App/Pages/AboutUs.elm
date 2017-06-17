f30b2e4e388bf249aa2cf096fb80ac3457887476
Daniel Wehner <dawehner@googlemail.com>
2017-06-17 09:03:34 +0100

    Some about us description

--- src/App/Pages/AboutUs.elm
+++ src/App/Pages/AboutUs.elm
@@ -7,4 +7,20 @@ import Html.Attributes exposing (src)
 
 view : Model -> Html Msg
 view model =
-    text "About us ... not implemented yet"
+    div []
+        [ h2 [] [ text "Contenta" ]
+        , text "Contenta is an API-First Drupal distribution. It's all about the content"
+        , h3 [] [ text "Elm Frontend" ]
+        , div []
+            [ div [] [ text "This frontend is using elm to render the output." ]
+            , div [] [ text "Elm is a delightful language for reliable webapps." ]
+            , div [] [ text "Generate JavaScript with great performance and no runtime exceptions." ]
+            , div [] [ text "Rendering an image works basically like HTML:" ]
+            , div []
+                [ code [] [ text "<img src=\"http://example.com/example.jpg\"></img>" ]
+                ]
+            , div []
+                [ code [] [ text "img [src \"http://example.com/example.jpg\"] [] " ]
+                ]
+            ]
+        ]
